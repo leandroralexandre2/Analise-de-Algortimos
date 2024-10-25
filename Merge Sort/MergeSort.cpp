38bd16a384579d39cc2f8a05737a20ea9c54ce77
Leandro Alexandre <117519583+leandroralexandre2@users.noreply.github.com>
2024-10-25 20:10:22 -0300

    Add files via upload

---
+++ Merge Sort/MergeSort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <climits> 
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+int main {
+	
+int INF = INT_MAX;
+int n;
+int *vetor
+int i, j;
+cin >> n;
+vetor = new int[n];
+cout << "Vetor criado com" <<n<<"posicoes!"<<endl;
+
+merge_sort(vetor, 0, n-1);
+//exibe os elementos 
+
+void merge(int *a, int p, int q, int t);
+void merge_sort(int *a, int p, int r);
+
+int n1, n2, k;
+int l[], r[];
+
+n1 = q - p+1;
+n2 = r-q;
+for (i =1; i <=n1; i++){
+  l[i] = a[p + i - 1];
+}
+for (j = 1; j<=n2; i++){
+
+  r[i]= a[q + j];
+l[n1+1] = 8;
+r[n2+1] = 8;
+i = 1;
+j = 1;
+}
+for (k = p; p <= r){
+ if l[i] = r[j]; {
+       a[k] = l[i]
+       i = i+1  }
+ else (a[k] =r[j] {
+      j=j+1;}
+}
+}
+
